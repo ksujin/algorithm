0a44940d0beefb7b971c120024cf09fc527c5019
강수진 <rkdthd1234@naver.com>
2018-11-24 01:37:56 +0900

    2739 구구

---
+++ src/com/sopt/algorithm/B_2739.java
@@ -0,0 +1,18 @@
+
+package com.sopt.algorithm;
+
+import java.util.Scanner;
+
+public class B_2739 {
+    public static void main(String args[]){
+
+        Scanner scn = new Scanner(System.in);
+
+        int a = scn.nextInt();
+        for (int b = 1; b <10 ; b++) {
+
+            System.out.println(a + " * " + b + " = " + a*b);
+
+        }
+    }
+}
