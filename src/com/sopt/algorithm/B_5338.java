52499bf8881c85ad017f76a93a917de76c36790f
강수진 <rkdthd1234@naver.com>
2018-12-03 06:26:28 +0900

    5338 마이크로소프트 로고

---
+++ src/com/sopt/algorithm/B_5338.java
@@ -0,0 +1,15 @@
+package com.sopt.algorithm;
+
+public class B_5338 {
+    public static void main(String args[]){
+
+
+        String ans = "       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|";
+        System.out.println(ans);
+    }
+}
+
