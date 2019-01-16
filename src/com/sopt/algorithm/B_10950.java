9a15ae486cccf381c3597c2077f038e324cc2e03
강수진 <rkdthd1234@naver.com>
2019-01-16 16:43:53 +0900

    10950 A+B -3

--- src/com/sopt/algorithm/B_10950.java
+++ src/com/sopt/algorithm/B_10950.java
@@ -1,4 +1,16 @@
 package com.sopt.algorithm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class B_10950 {
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.valueOf(br.readLine());
+        while (testCase-- > 0){
+            String[] arr = br.readLine().split(" ");
+            System.out.println(Integer.valueOf(arr[0])+Integer.valueOf(arr[1]));
+        }
+    }
 }
